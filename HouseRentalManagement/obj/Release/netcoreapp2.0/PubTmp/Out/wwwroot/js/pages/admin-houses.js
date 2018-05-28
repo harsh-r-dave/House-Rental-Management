9b88891084c7cb3f58e63ca88087718dc1057cea
harsh18594 <harsh18594@gmail.com>
2018-05-28 19:35:44 -0400

    refactored home page layout

---
+++ HouseRentalManagement/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/pages/admin-houses.js
@@ -0,0 +1,49 @@
+﻿var Hrm = Hrm || {};
+
+Hrm.AdminHouses = function () {
+    var viewModel;
+
+    var initDataTables = function () {
+        $('#house-list-table').dataTable({
+            "sDom": '<"top"f>rt<"bottom"lp><"clear">',
+            "columnDefs": [{
+                "targets": [2],
+                "orderable": false
+            }]
+        });
+	};
+
+	var deleteHouse = function (id) {
+		var link = '/Admin/DeleteHouse/' + id;
+		swal({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#337ab7',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, delete it!'
+		}).then(function (result) {
+			if (result.value) {
+				window.location.href = link;
+			}
+		});
+	};
+
+    var initPage = function () {
+        initDataTables();
+    };
+
+    var init = function (model) {
+        console.log("Hrm.AdminHouses init()");
+        viewModel = model;
+
+        initPage();
+    };
+
+    var oPublic = {
+		init: init,
+		deleteHouse: deleteHouse
+    }
+    return oPublic;
+}();
--- HouseRentalManagement/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/pages/house-index.js
+++ HouseRentalManagement/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/pages/house-index.js
@@ -5,20 +5,29 @@ Hrm.HouseIndex = function () {
 
     var bindEvents = function () { };
 
-    var initFullPage = function () {
-        $('#main').fullpage({
-            scrollingSpeed: 1000,
-            autoScrolling: true,
-            fitToSection: true,
-            fitToSectionDelay: 2000
-        });
-    };
+    //var initFullPage = function () {
+    //    $('#main').fullpage({
+    //        scrollingSpeed: 1000,
+    //        autoScrolling: true,
+    //        fitToSection: true,
+    //        fitToSectionDelay: 2000
+    //    });
+    //};
+
+	var initSmoothScroll = function () {
+		$('.smooth-scroll').on('click', function () {
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
+			}, 1500);
+		});
+	};
 
     var init = function (model) {
         console.log("Hrm.HouseIndex init()");
         viewModel = model;
 
-        initFullPage();
+		//initFullPage();
+		initSmoothScroll();
     };
 
     var oPublic = {
