61b6b735a2ce4052b38b1732b4513b9584e7d8c7
harsh18594 <harsh18594@gmail.com>
2018-06-23 18:07:20 -0400

    improved UX for house images

--- HouseRentalManagement/wwwroot/js/pages/house-info.js
+++ HouseRentalManagement/wwwroot/js/pages/house-info.js
@@ -83,14 +83,23 @@ Hrm.HouseInfo = function () {
 	var initSlickCarousel = function () {
 		$('#house-images-container').slick({
 			autoplay: true,
-			autoplaySpeed: 2000,
+			autoplaySpeed: 5000,
 			variableWidth: true,
 			centerMode: true,
 			centerPadding: '60px',
 			slidesToShow: 3,
 			pauseOnFocus: false,
+			focusOnSelect: true,
 			prevArrow: '<span class="slick-custom-nav slick-left-container"><i class="fa fa-arrow-circle-left slick-custom-left"></i></span>',
 			nextArrow: '<span class="slick-custom-nav slick-right-container"><i class="fa fa-arrow-circle-right slick-custom-right"></i></span>'
+		});
+
+		$('#house-images-container').on('afterChange', function (event, slick, currentSlide) {
+			var slideHtml = $(slick.$slides.get(currentSlide)).html();
+			var image = $('img', slideHtml);
+			var imageSrc = $(image).attr('src');
+			$('#main-image-anchor').attr('href', imageSrc);
+			$('#main-image').attr('src', imageSrc);
 		});
 	};
 
