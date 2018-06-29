24a037dff09cf219b6058b92a7ba298643b84152
harsh18594 <harsh18594@gmail.com>
2018-06-29 19:39:31 -0400

    committing progress on contact us

--- HouseRentalManagement/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/pages/house-index.js
+++ HouseRentalManagement/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/pages/house-index.js
@@ -22,10 +22,36 @@ Hrm.HouseIndex = function () {
 		});
 	};
 
+	var setLoader = function () {
+		$('#contact-us-loader-container #sending-message').show();
+		$('#contact-us-loader-container #message-sent').hide();
+		$('#contact-us-loader-container #message-failed').hide();
+		$('#contact-us-form').hide();
+	};
+	var setSent = function () {		
+		$('#contact-us-loader-container #sending-message').hide();
+		$('#contact-us-loader-container #message-sent').show();
+		$('#contact-us-loader-container #message-failed').hide();
+		$('#contact-us-form').hide();
+	};
+	var setFailed = function () {
+		$('#contact-us-loader-container #sending-message').hide();
+		$('#contact-us-loader-container #message-sent').hide();
+		$('#contact-us-loader-container #message-failed').show();
+		$('#contact-us-form').hide();
+	};
+	var clearLoader = function () {
+		$('#contact-us-loader-container #sending-message').hide();
+		$('#contact-us-loader-container #message-sent').hide();
+		$('#contact-us-loader-container #message-failed').hide();
+		$('#contact-us-form').show();
+	};	
+
 	var submitInquiry = function () {
 		$('#contact-us-form').on('submit', function (e) {
 			e.preventDefault();
 			if ($('#contact-us-form').valid()) {
+				setLoader();
 				var name = $('#AddInquiryViewModel_Name').val();
 				var phone = $('#AddInquiryViewModel_PhoneNumber').val();
 				var email = $('#AddInquiryViewModel_Email').val();
@@ -41,18 +67,46 @@ Hrm.HouseIndex = function () {
 					}
 				}).done(function (result) {
 					if (result.success) {
-						toastr.success('Your inquiry has been submitted succesfully.', '', Hrm.Toastr.config);
+						setTimeout(function () {
+							clearLoader();
+							setSent();
+						}, 750);						
 					}
 					else {
 						toastr.error(result.error, '', Hrm.Toastr.config);
-					}
+						setTimeout(function () {
+							clearLoader();
+							setFailed();
+						}, 750);							
+					}					
 				}).fail(function (jqXHR, textStatus) {
 					console.log(textStatus);
+					setTimeout(function () {
+						clearLoader();
+						setFailed();
+					}, 750);	
 				});
 			}
 		});
 	};
 
+	var validatePhone = function () {
+		$("#AddInquiryViewModel_PhoneNumber").keydown(function (e) {
+			// Allow: backspace, delete, tab, escape, enter and .
+			if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+				// Allow: Ctrl+A, Command+A
+				(e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+				// Allow: home, end, left, right, down, up
+				(e.keyCode >= 35 && e.keyCode <= 40)) {
+				// let it happen, don't do anything
+				return;
+			}
+			// Ensure that it is a number and stop the keypress
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+				e.preventDefault();
+			}
+		});
+	};
 	var init = function (model) {
 		console.log("Hrm.HouseIndex init()");
 		viewModel = model;
@@ -60,10 +114,12 @@ Hrm.HouseIndex = function () {
 		//initFullPage();
 		initSmoothScroll();
 		submitInquiry();
+		validatePhone();
 	};
 
 	var oPublic = {
-		init: init
+		init: init,
+		clearLoader: clearLoader
 	}
 	return oPublic;
 }();
